8e066b4c16285e2cd247e4dec21b4946c7f08c7d
Fahim Kabir <fkabir101@gmail.com>
2019-11-08 17:40:50 -0500

    working

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -2,9 +2,6 @@ import axios from "axios";
 
 export default {
   placeOrder: function (orderBody){
-    return axios.post('/paypal/pay', orderBody).then( (response) => {      
-        // window.location.href= response.data; 
-        console.log(response);
-    })
+    return axios.post('/paypal/pay', orderBody)
   }
 }
