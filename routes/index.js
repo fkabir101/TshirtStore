075aaef713447d823ea65dce1782352d53b1c55b
Fahim Kabir <fkabir101@gmail.com>
2019-11-15 14:51:26 -0500

    added client build route

--- routes/index.js
+++ routes/index.js
@@ -8,7 +8,7 @@ const userRoutes = require('./user')
 router.use('/orders', orderRoutes);
 router.use('/user', userRoutes);
 
-// router.use(function(req, res) {
-//   res.sendFile(path.join(__dirname, "../client/build/index.html"));
-// });
+router.use(function(req, res) {
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+});
 module.exports = router;
